c895cdd19882e2304b35f84db114beca0bc7c00e
Bruce Perens <bruce@perens.com>
2020-12-07 23:09:32 -0800

    Start making the macro emit correctly.

--- src/macro.cr
+++ src/macro.cr
@@ -1,39 +1,47 @@
 module Internationalize
+  
   # Translation functions.
   extend self
 
-  macro t(string, name = nil)
-    {% if string.class_name == "StringInterpolation" %}
-      {% arguments = [] of StringLiteral %}
-      {% s = "" %}
+  macro t(original, name = nil)
+    {% if original.class_name == "StringInterpolation" %}
+      {% expressions = [] of StringLiteral %}
+      {% native = "" %}
       {% count = 0 %}
-      {% for e, index in string.expressions %}
+      {% for e, index in original.expressions %}
         {% if e.class_name == "StringLiteral" %}
-          {% s += e.gsub(/\$([0-9])/, "$$\\1") %}
+          {% native += e.gsub(/\$([0-9])/, "$$\\1") %}
         {% elsif e.class_name == "Call" %}
-          {% a = e.args %}
+          {% arguments = e.args %}
           {% if e.named_args %}
-            {% a += e.named_args %}
+            {% arguments += e.named_args %}
           {% end %}
           {% if e.receiver %}
             {% receiver = "#{e.receiver}.".id %}
           {% else %}
             {% receiver = "" %}
           {% end %}
-          {% arguments.push("#{receiver}#{e.name}(#{a.splat})".id) %}
-          {% s += "$#{count}" %}
+          {% expressions.push("#{receiver}#{e.name}(#{arguments.splat})".id) %}
+          {% native += "$#{count}" %}
           {% count += 1 %}
         {% else %}
-          {% raise "#{string} at #{string.filename}:#{string.line_number}: #{e.class_name} in interpolated string." %}
+          {% raise "#{original} at #{original.filename}:#{original.line_number}: #{e.class_name} in interpolated string." %}
         {% end %}
       {% end %}
-      # From {{string.id}} at {{string.filename.id}}:{{string.line_number}}
-      {{ %Q[translate("#{s.id}", #{arguments.id}, #{name})].id }}
-    {% elsif string.class_name == "StringLiteral" %}
-      # From {{string.id}} at {{string.filename.id}}:{{string.line_number}}
-      {{ %Q[translate("#{string.id}", nil, #{name})].id }}
+      # From {{original.id}} at {{original.filename.id}}:{{original.line_number}}
+      {{ %Q[translate("#{native.id}", #{expressions.id}, :#{name.id.stringify.gsub(/^"|"$/, "")})].id }}
+        
+    {% elsif original.class_name == "StringLiteral" %}
+      {% native = original; expressions = nil %}
     {% else %}
-      {% raise "t(#{string}) at #{string.filename}:#{string.line_number}: the argument must be a literal string, not a variable or expression." %}
+      {% raise "t(#{original}) at #{original.filename.id}:#{original.line_number}: the argument must be a literal string, not a variable or expression." %}
+    {% end %}
+    {% name ||= native %}
+    {{ comment = "# From #{original} at #{original.filename.id}:#{original.line_number}" }}
+    translate({{native.id.stringify}}, {{expressions}}, :{{name.id.stringify.gsub(/^"|"$/, "")}})
+    {% if flag?(:"emit-translation-strings") %}
+      {% puts comment %}
+      {% puts %Q[:#{name.id.stringify.gsub(/^"|"$/, "")} = { #{native}, :#{original.filename}, #{original.line_number} }].id %}
     {% end %}
   end
 end
